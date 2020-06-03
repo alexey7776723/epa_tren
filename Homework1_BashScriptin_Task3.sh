a09c722644dca98b9953acc595989ec9191a4947
Alexey <7776723@mail.ru>
2020-06-03 13:35:38 +0300

    All tasks

---
+++ Homework1_BashScriptin_Task3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." > lorem
+
+grep dolore lorem
--- Homework3_BashScriptin_Task1.sh
+++ Homework3_BashScriptin_Task1.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-unset ss
-unset leanYearMonths
 leanYearMonths=(31 29 31 30 31 30 31 31 30 31 30 31)
-#for i in ${leanYearMonths[@]}
-#  do 
-#    if [[ ss=$(($ss + $i)) -ge $(date +"%j") ]]
-#    then 
-#      echo "Number days of current month: $i"
-#      break
-#    fi
-#done
 
 for i in ${leanYearMonths[@]}
   do 
